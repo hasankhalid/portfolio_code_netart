6c9751b0e599d5d11b991ff8b6f3045a064c37d3
Hasan Khalid <hasankhalid940@gmail.com>
2022-09-21 21:18:28 -0400

    animation part 3

--- src/components/home/landing.js
+++ src/components/home/landing.js
@@ -1,8 +1,25 @@
-import styles from './css-modules/landing.module.css'
+import styles from './css-modules/landing.module.css';
+import { useRef, useEffect } from 'react';
 
 function Landing({ heading, paras }) {
+
+  let containerRef = useRef();
+  let headingRef = useRef();
+
+  useEffect(() => {
+    containerRef.current.addEventListener('mousemove', (event) => {
+
+      let diffX = headingRef.current.getBoundingClientRect().x - event.clientX; 
+      let diffY = headingRef.current.getBoundingClientRect().y - event.clientY; 
+
+      headingRef.current.style.transform = `translate3d(${Math.round(diffX * 0.025)}px, ${Math.round(diffY * 0.025)}px, 0px)`;
+    });
+  }, []);
+
+
+
   return (
-    <div className={styles.container}>
+    <div ref={containerRef} className={styles.container}>
       <div className={styles.shape_container}>
         <div className={styles.container_50}>
           <div className={styles.circle_projects}>
@@ -17,8 +34,7 @@ function Landing({ heading, paras }) {
           <div className={styles.circle_resume}>
           </div>
         </div>
-
-        <div className={styles.heading_container}>
+        <div ref={headingRef} className={styles.heading_container}>
           <h1 className={styles.heading}>{heading}</h1>
         </div>
       </div>
